0d744aa72d156f507f7536c9d20de345a1f13ebc
track-frags <jadirks@anamosa.k12.ia.us>
2026-03-25 15:35:01 +0000

    string

--- Printer.cs
+++ Printer.cs
@@ -5,6 +5,16 @@ namespace knightmoves;
 public class Printer{
     
     public string Print(string dateString){
+        DateTime now = DateTime.Now;
+        Display($"Now: {now}");
+
+        string DataString = "2024-06-01T12:00:00";
+        DateTime date = DateTime.Parse(dateString);
+        Display($"Parsed Date: {date}");
+
+        int year = date.Year;
+        Display($"Parsed Year: {year}");
+
         return "";
     }
 }
